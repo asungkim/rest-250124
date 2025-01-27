a082aebde974884411e662ab427f3a5044547ea5
asungkim <kimasung143@gmail.com>
2025-01-27 09:42:11 +0900

    rsdata add, modify transactional

---
+++ src/main/java/com/example/rest/global/dto/RsData.java
@@ -0,0 +1,11 @@
+package com.example.rest.global.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class RsData {
+    private String code;
+    private String msg;
+}

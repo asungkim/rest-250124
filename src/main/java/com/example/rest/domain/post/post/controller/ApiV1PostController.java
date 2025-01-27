1efd114d9a6694ad7d23e8689cef8e2ae3982c61
asungkim <asungkim007@naver.com>
2025-01-27 09:42:11 +0900

    rsdata add, modify transactional

--- src/main/java/com/example/rest/domain/post/post/controller/ApiV1PostController.java
+++ src/main/java/com/example/rest/domain/post/post/controller/ApiV1PostController.java
@@ -2,14 +2,13 @@ package com.example.rest.domain.post.post.controller;
 
 import com.example.rest.domain.post.post.entity.Post;
 import com.example.rest.domain.post.post.service.PostService;
+import com.example.rest.global.dto.RsData;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 @RestController
 @RequestMapping("/api/v1/posts")
@@ -28,15 +27,13 @@ public class ApiV1PostController {
     }
 
     @DeleteMapping("{id}")
-    public Map<String,Object> delete(@PathVariable long id) {
+    public RsData delete(@PathVariable long id) {
         Post post = postService.getItem(id).get();
         postService.delete(post);
 
-        Map<String, Object> rsData=new HashMap<>();
-        rsData.put("code","200-1");
-        rsData.put("msg","%d번 글 삭제가 완료되었습니다.".formatted(id));
-
-        return rsData;
+        return new RsData(
+                "200-1",
+                "%d번 글 삭제가 완료되었습니다.".formatted(id));
     }
 
     @AllArgsConstructor
@@ -47,15 +44,13 @@ public class ApiV1PostController {
     }
 
     @PutMapping("{id}")
-    public Map<String,Object> modify(@PathVariable long id,@RequestBody ModifyForm form) {
+    public RsData modify(@PathVariable long id, @RequestBody ModifyForm form) {
         Post post = postService.getItem(id).get();
         postService.modify(post,form.getTitle(),form.getContent());
 
-        Map<String, Object> rsData=new HashMap<>();
-        rsData.put("code","200-1");
-        rsData.put("msg","%d번 글 수정이 완료되었습니다.".formatted(id));
-
-        return rsData;
+        return new RsData(
+                "200-1",
+                "%d번 글 수정이 완료되었습니다.".formatted(id));
     }
 
 }

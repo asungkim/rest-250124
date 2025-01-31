a30c30860b7db8fbc28977764d15fc2b323e50ef
asungkim <asungkim007@naver.com>
2025-01-31 09:56:14 +0900

    RsData getStatusCode apply in AOP

--- src/main/java/com/example/rest/global/dto/RsData.java
+++ src/main/java/com/example/rest/global/dto/RsData.java
@@ -12,7 +12,13 @@ public class RsData<T> {
     private String msg;
     private T data;
 
-    public RsData(String msg, String code) {
+    public RsData(String code, String msg) {
         this(code,msg,null);
+    }
+
+    public int getStatusCode() {
+        String statusCodeStr = this.code.split("-")[0];
+        System.out.println("stCode = "+statusCodeStr);
+        return Integer.parseInt(statusCodeStr);
     }
 }

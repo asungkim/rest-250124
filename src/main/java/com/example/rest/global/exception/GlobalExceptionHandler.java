1d586d490be3b992c1aa2932eb3876b462ba0a5a
asungkim <asungkim007@naver.com>
2025-01-31 11:42:14 +0900

    printStackTrace apply

--- src/main/java/com/example/rest/global/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/rest/global/exception/GlobalExceptionHandler.java
@@ -4,19 +4,19 @@ import com.example.rest.global.dto.RsData;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.util.NoSuchElementException;
-import java.util.stream.Collectors;
 
 @RestControllerAdvice
 @RequiredArgsConstructor
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(NoSuchElementException.class)
-    public ResponseEntity<RsData<Void>> handleNoSuchElementException() {
+    public ResponseEntity<RsData<Void>> handleNoSuchElementException(NoSuchElementException e) {
+
+        e.printStackTrace();
 
         return ResponseEntity
                 .status(HttpStatus.NOT_FOUND)
@@ -27,20 +27,20 @@ public class GlobalExceptionHandler {
 
     }
 
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<RsData<Void>> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
-
-        String msg = e.getBindingResult().getFieldErrors()
-                .stream()
-                .map(f -> f.getField() + " : " + f.getCode() + " : " + f.getDefaultMessage())
-                .sorted()
-                .collect(Collectors.joining("\n"));
-
-        return ResponseEntity
-                .status(HttpStatus.BAD_REQUEST)
-                .body(new RsData<>(
-                        "400-1",
-                        msg
-                ));
-    }
+//    @ExceptionHandler(MethodArgumentNotValidException.class)
+//    public ResponseEntity<RsData<Void>> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+//
+//        String msg = e.getBindingResult().getFieldErrors()
+//                .stream()
+//                .map(f -> f.getField() + " : " + f.getCode() + " : " + f.getDefaultMessage())
+//                .sorted()
+//                .collect(Collectors.joining("\n"));
+//
+//        return ResponseEntity
+//                .status(HttpStatus.BAD_REQUEST)
+//                .body(new RsData<>(
+//                        "400-1",
+//                        msg
+//                ));
+//    }
 }

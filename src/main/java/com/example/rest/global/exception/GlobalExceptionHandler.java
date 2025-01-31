eef674e9716210bf783f83b14b6325ca7dba7e20
asungkim <kimasung143@gmail.com>
2025-01-31 11:17:21 +0900

    error getBindingResult apply

--- src/main/java/com/example/rest/global/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/rest/global/exception/GlobalExceptionHandler.java
@@ -2,7 +2,9 @@ package com.example.rest.global.exception;
 
 import com.example.rest.global.dto.RsData;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -13,14 +15,28 @@ import java.util.NoSuchElementException;
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(NoSuchElementException.class)
-    public ResponseEntity<RsData<Void>> handle() {
+    public ResponseEntity<RsData<Void>> handleNoSuchElementException() {
 
         return ResponseEntity
-                .status(404)
+                .status(HttpStatus.NOT_FOUND)
                 .body(new RsData<>(
                         "404-1",
                         "해당 데이터가 존재하지 않습니다."
                 ));
 
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<RsData<Void>> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+
+        String field = e.getBindingResult().getFieldError().getField();
+        String msg = e.getBindingResult().getFieldError().getDefaultMessage();
+
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(new RsData<>(
+                        "400-1",
+                        field + " : " + msg
+                ));
     }
 }
